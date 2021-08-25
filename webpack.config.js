60587fc979319461008c08dd4a09a4722dfb7fe1
alan <alangu.ua@gmail.com>
2021-08-25 12:30:27 -0400

    Installed all dependencies

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'eval-cheap-module-source-map',
+  devServer: {
+    // contentBase: path.join(__dirname, 'public')
+    static: './'
+  }
+}

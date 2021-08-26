303a35cac02be6904d39099f22ddf8787e186d59
alan <alangu.ua@gmail.com>
2021-08-25 23:02:27 -0400

    Created Food, Drink, Movie

--- client/src/components/Drink.jsx
+++ client/src/components/Drink.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 
-var Drink = (props) => {
-  return (
-    <h1>This is where the Drink will go</h1>
-  );
+class Drink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drink: {}
+    };
+  }
+  componentDidMount() {
+    axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+      .then((res) => {
+        var drinkObject = {
+          name: res.data.drinks[0].strDrink,
+          thumbnail: res.data.drinks[0].strDrinkThumb,
+          Instructions: res.data.drinks[0].strInstructions,
+          glass: res.data.drinks[0].strGlass,
+          ingredients: []
+        };
+        for (var index = 1; index <= 15; index++) {
+          var ingredientStr = 'strIngredient' + index;
+          var measurementStr = 'strMeasure' + index;
+          if (res.data.drinks[0][ingredientStr] !== null) {
+            if (res.data.drinks[0][measurementStr] !== null) {
+              drinkObject.ingredients.push(res.data.drinks[0][measurementStr]
+                + ' ' + res.data.drinks[0][ingredientStr]);
+            } else {
+              drinkObject.ingredients.push(res.data.drinks[0][ingredientStr]);
+            }
+          }
+          if (res.data.drinks[0][measurementStr] === null && res.data.drinks[0][ingredientStr] === null) {
+            break;
+          }
+        }
+        this.setState({ drink: drinkObject });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div id='temp'>
+        <div id='imageContainer'><img src={this.state.drink.thumbnail} /></div>
+        <div id="informationContainer">
+          <p>{this.state.drink.name}</p>
+          <p>{this.state.drink.Instructions}</p>
+          {this.state.drink.ingredients ?
+            this.state.drink.ingredients.map((ingredient) => { return (<li key={ingredient}>{ingredient}</li>) })
+            : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Drink;
--- client/src/components/Food.jsx
+++ client/src/components/Food.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import axios from 'axios';
 
-var Food = (props) => {
-  return (
-    <h1>This is where the food will go</h1>
-  );
+class Food extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      food: {}
+    };
+  }
+  componentDidMount() {
+    axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((res) => {
+        var foodObject = {
+          name: res.data.meals[0].strMeal,
+          thumbnail: res.data.meals[0].strMealThumb,
+          instructions: res.data.meals[0].strInstructions,
+          ingredients: []
+        };
+        for (var index = 1; index <= 20; index++) {
+          var strIngredient = 'strIngredient' + index;
+          var strMeasure = 'strMeasure' + index;
+          if (res.data.meals[0][strIngredient] !== '') {
+            if (res.data.meals[0][strMeasure] === '' && res.data.meals[0][strIngredient] === '') {
+              break;
+            }
+            if (res.data.meals[0][strMeasure] !== '') {
+              foodObject.ingredients.push(res.data.meals[0][strMeasure] + ' ' + res.data.meals[0][strIngredient]);
+            } else {
+              foodObject.ingredients.push(res.data.meals[0][strIngredient]);
+            }
+          }
+        }
+        this.setState({ food: foodObject });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div id='temp'>
+        <div id="informationContainer">
+          <p>{this.state.food.name}</p>
+          {/* <p>{this.state.food.instructions}</p> */}
+          <p>Ingredients</p>
+          {this.state.food.ingredients ?
+            this.state.food.ingredients.map((ingredient) => { return (<li key={ingredient}>{ingredient}</li>) })
+            : null}
+        </div>
+        <div id='imageContainer'><img src={this.state.food.thumbnail} /></div>
+      </div>
+    );
+  }
 }
-
 export default Food;
